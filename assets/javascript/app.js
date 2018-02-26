d4622a46ea73c057ff8c3fb3fe6b75e4e0efba4c
ntdang <ndang83@gmail.com>
2018-02-26 12:47:38 -0500

    Able to load first question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,33 +12,28 @@ $('.document').ready(function () {
   var images = ["../images/Feeny.jpg", "../images/TommyPickles.png", "../images/Jagged.jpg", "../images/Legends.jpg", "../images/SMK.jpg"];
 
 
-  var triviaQuestions = {
-    q1: {
-      question: "Mr. Feeny was the teacher in which popular TV show?",
-      answerList: ["Fresh Prince of Bel-Air", "Beverly Hills 90210", "Boy Meets Word", "Saved by the Bell"],
-      answer: 2,
-      q2: {
-        question: "What was Tommy's last name in Rugrats?",
-        answerList: ["Finster", "Pickles", "Chuckie", "DeVille"],
-        answer: 1,
-        q3: {
-          question: "Jagged Little Pill is the album by which singer?",
-          answerList: ["Alanis Morissette", "Whitney Houston", "Mariah Carey", "Courtney Love"],
-          answer: 0,
-          q4: {
-            question: "The gameshow where teams competed to find lost treasures in a Mayan temple was called..?",
-            answerList: ["Secrets of a Lost Temple", "The Mayan Maze", "What Would You Do?", "Legends of the Hidden Temple"],
-            answer: 3,
-            q5: {
-              question: "Which of the following was NOT a character in Super Mario Kart?",
-              answerList: ["Luigi", "Peach", "Crash", "Toad"],
-              answer: 2,
-            }
-          }
-        }
-      }
-    }
-  };
+  var triviaQuestions = [{
+    question: "Mr. Feeny was the teacher in which popular TV show?",
+    answerList: ["Fresh Prince of Bel-Air", "Beverly Hills 90210", "Boy Meets Word", "Saved by the Bell"],
+    answer: 2,
+  }, {
+    question: "What was Tommy's last name in Rugrats?",
+    answerList: ["Finster", "Pickles", "Chuckie", "DeVille"],
+    answer: 1,
+  }, {
+    question: "Jagged Little Pill is the album by which singer?",
+    answerList: ["Alanis Morissette", "Whitney Houston", "Mariah Carey", "Courtney Love"],
+    answer: 0,
+  }, {
+    question: "The gameshow where teams competed to find lost treasures in a Mayan temple was called..?",
+    answerList: ["Secrets of a Lost Temple", "The Mayan Maze", "What Would You Do?", "Legends of the Hidden Temple"],
+    answer: 3,
+  }, {
+    question: "Which of the following was NOT a character in Super Mario Kart?",
+    answerList: ["Luigi", "Peach", "Crash", "Toad"],
+    answer: 2,
+  }];
+
 
   // Messages Object
   var messages = {
@@ -54,6 +49,8 @@ $('.document').ready(function () {
   // On-click functions to start and start-over
   $("#start-game").on("click", function () {
     $(this).hide();
+    $("#timer").html("<h3>Time Remaining: " + timer + " seconds</h3>");
+    game.loadQuestion();
   });
 
   // Game object
@@ -67,11 +64,20 @@ $('.document').ready(function () {
 
     // Time remaining function
     countDown: function () {
-      timer--;
-      $("#timer").text("Time remaining: " + timer);
+      game.timer--;
+      $("#timer").text("Time remaining: " + game.timer);
 
-      if (timer === 0) {
+      if (game.timer === 0) {
         $("#timer").text("Time's up!");
+      }
+    },
+
+    //loadQuestion function
+    loadQuestion: function () {
+      timer: setInterval(game.timer, 1000);
+      $("#current-question").html("<h2>" + triviaQuestions[this.currentQuestion].question + "</h2>");
+      for (var i = 0; i < triviaQuestions[this.currentQuestion].answerList.length; i++) {
+        $("#answer-choices").append("<button class='answer-button' id='button'" + "data-name'" + triviaQuestions[this.currentQuestion].answerList[i] + "'>" + triviaQuestions[this.currentQuestion].answerList[i] + "</button>");
       }
     },
 
